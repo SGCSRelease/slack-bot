fdc07d02ee053e763f35bda7ccb0bc9441546b6a
JimJeon <jimjeonme@gmail.com>
2017-07-17 22:35:23 +0900

    hosts_deny.py -> add_ip.py in commands

--- bot/commands/hosts_allow.py
+++ bot/commands/hosts_allow.py
@@ -6,11 +6,11 @@ from ..app import slack
         team_id='T3QDD12QK', methods=['POST'])
 def allow(**kwargs):
     target = kwargs.get('text')
-    add_py = subprocess.Popen(('/usr/bin/sudo sbin/hosts_deny.py %s %s'
+    add_py = subprocess.Popen(('/usr/bin/sudo sbin/add_ip.py %s %s'
         % ('/etc/hosts.allow', target)).split(), shell=False)
     add_py.wait(timeout=10)
 
     remove_py = subprocess.Popen(('/usr/bin/sudo sbin/remove_ip.py %s %s'
         % ('/etc/hosts.deny', target)).split(), shell=False)
     remove_py.wait(timeout=10)
-    return slack.response(target + ' banned', response_type='in_channel')
+    return slack.response(target + ' unbanned', response_type='in_channel')
--- bot/commands/hosts_deny.py
+++ bot/commands/hosts_deny.py
@@ -7,7 +7,7 @@ from ..app import slack
                team_id='T3QDD12QK', methods=['POST'])
 def deny(**kwargs):    
     target = kwargs.get('text')
-    add_py = subprocess.Popen(('/usr/bin/sudo sbin/hosts_deny.py %s %s'
+    add_py = subprocess.Popen(('/usr/bin/sudo sbin/add_ip.py %s %s'
         % ('/etc/hosts.deny', target)).split(), shell=False)
     add_py.wait(timeout=10)
 
